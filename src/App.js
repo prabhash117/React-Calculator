2cf2ee65c276da021884b22e24a9816ec2c8742a
prabhash117 <prabhash.singh117@gmail.com>
2020-04-10 10:30:22 +0530

    Reduced LOC Count
    Updated the code to reduce LOC count and deregister keydown event listener.

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ export default class App extends Component{
   componentDidMount(){
     document.addEventListener('keydown',this.buttonPress)
   }
+  
   buttonPress(event){
     for(let i=0;i<buttonData.length;i++){
         if(buttonData[i].keyCode===event.keyCode){
@@ -32,48 +33,40 @@ export default class App extends Component{
     }
   }
   onButtonClick(value){
-    let prevText = this.state.ans
+    let prevText = this.state.ans;
+    let finalAns;
     if(value===Esc){
-      this.setState({
-          ans:'0',
-      })
+      finalAns = '0';
     }else if(value===CE){
-      let newAns = (prevText.length<=1)? '0' :prevText.substring(0, prevText.length-1);
-      this.setState({
-          ans:newAns,
-      })
+      finalAns = (prevText.length<=1)? '0' :prevText.substring(0, prevText.length-1);
     }else if(value===Divi || value===Sub || value===Add || value===mult){
       const opretorExist = prevText.charAt(prevText.length-1);
       if(opretorExist===Divi || opretorExist===Sub || opretorExist===Add || opretorExist===mult){
         let newAns = (prevText.length<=1)? 0 :prevText.substring(0, prevText.length-1);
-        this.setState({
-            ans:newAns += value,
-        })
+        finalAns = newAns += value;
       }else{
-        this.setState({
-          ans:prevText += value,
-      })
+        finalAns = prevText += value;
       }
     }else if(value===equl){
       const lastOpretor = prevText.charAt(prevText.length-1);
       if(lastOpretor===Divi || lastOpretor===Sub || lastOpretor===Add || lastOpretor===mult){
         return false;
       }else{
-        this.setState({
-          ans:eval(prevText).toString(),
-        })
+        finalAns = eval(prevText).toString();
       }
     }else if(value===point){
       if(prevText.lastIndexOf('.')=== -1){
-        this.setState({
-          ans:prevText += value,
-        })
+        finalAns = prevText += value;
       }
     }else{
-      this.setState({
-        ans:(prevText==='0')? value : prevText += value,
-      })
+      finalAns = (prevText==='0')? value : prevText += value;
     }
+    this.setState({
+      ans:finalAns,
+    })
+  }
+  componentWillUnmount(){
+    document.removeEventListener('keydown',this.buttonPress)
   }
   render(){
     const {ans} = this.state;
